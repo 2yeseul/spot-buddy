f0183ce3c91933e8dff4e095ca8315898fcd698a
이예슬 <20152917@sungshin.ac.kr>
2021-12-26 03:48:24 +0900

    chore : initial commit for refactoring

---
+++ src/main/kotlin/kr/co/spotbuddy/SpotBuddyApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.spotbuddy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotBuddyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpotBuddyApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/spotbuddy/SpotBuddyApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.spotbuddy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotBuddyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
