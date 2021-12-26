0c1bc7f941c8f5b51b8edc4bd85a670461ab4aeb
이예슬 <20152917@sungshin.ac.kr>
2021-12-26 23:11:09 +0900

    refactor : emailCheck

--- src/main/kotlin/kr/co/spotbuddy/exception/CustomException.kt
+++ src/main/kotlin/kr/co/spotbuddy/exception/CustomException.kt
@@ -15,4 +15,6 @@ enum class ExceptionDefinition(
     ALREADY_EXISTS_NICKNAME("이미 존재하는 닉네임 입니다.", HttpStatus.NOT_ACCEPTABLE),
     ALREADY_EXISTS_EMAIL("이미 존재하는 이메일 입니다.", HttpStatus.NOT_ACCEPTABLE),
     DELETE_ACCOUNT_WITHIN_10DAYS("10일 이내에 탈퇴한 계정입니다.", HttpStatus.NOT_ACCEPTABLE),
+    NOT_FOUND_USER("존재하지 않는 사용자 입니다.", HttpStatus.NOT_FOUND),
+    INVALID_EMAIL_TOKEN("유효하지 않은 이메일 토큰 입니다.", HttpStatus.FORBIDDEN),
 }
