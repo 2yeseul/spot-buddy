a165f013f1cb9017c47c35b59c7faddad1d33d04
이예슬 <20152917@sungshin.ac.kr>
2022-01-26 23:08:55 +0900

    refactor: alarm

---
+++ src/main/kotlin/kr/co/spotbuddy/domain/repository/AlarmRepository.kt
@@ -0,0 +1,14 @@
+package kr.co.spotbuddy.domain.repository
+
+import kr.co.spotbuddy.domain.Alarm
+import kr.co.spotbuddy.domain.Member
+import org.springframework.data.domain.Page
+import org.springframework.data.jpa.repository.JpaRepository
+import java.awt.print.Pageable
+
+interface AlarmRepository: JpaRepository<Alarm, Long> {
+    fun findAllByMemberOrderByIdDesc(member: Member, pageable: Pageable): Page<Alarm>
+    fun existsByMember(member: Member): Boolean
+    fun existsByMemberAndIsRead(member: Member, isRead: Boolean): Boolean
+    fun deleteAllByMember(member: Member): Unit
+}
