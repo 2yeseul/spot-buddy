89845e5dd15b27e717f0c3bc21538ae7fa80873f
이예슬 <20152917@sungshin.ac.kr>
2022-01-28 00:04:10 +0900

    refactor: block

--- src/main/kotlin/kr/co/spotbuddy/interfaces/BlockController.kt
+++ src/main/kotlin/kr/co/spotbuddy/interfaces/BlockController.kt
@@ -2,6 +2,7 @@ package kr.co.spotbuddy.interfaces
 
 import kr.co.spotbuddy.application.BlockResponse
 import kr.co.spotbuddy.application.BlockService
+import kr.co.spotbuddy.interfaces.request.BlockRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -25,5 +26,12 @@ class BlockController(
         val blocks = blockService.getMemberBlocks(memberId)
 
         return ResponseEntity.ok(blocks)
+    }
+
+    @PostMapping("/")
+    fun doBlockMember(@RequestBody blockRequest: BlockRequest): ResponseEntity<HttpStatus> {
+        blockService.doBlock(blockExecutor = blockRequest.blockExecutor, getBlocked = blockRequest.getBlocked)
+
+        return ResponseEntity.ok(HttpStatus.ACCEPTED)
     }
 }
