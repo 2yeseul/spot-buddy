e4e6fa36b72cec74282078894a0c7e291e8b6fc5
이예슬 <20152917@sungshin.ac.kr>
2022-01-27 23:52:13 +0900

    refactor: alarm

--- src/main/kotlin/kr/co/spotbuddy/application/AlarmService.kt
+++ src/main/kotlin/kr/co/spotbuddy/application/AlarmService.kt
@@ -1,9 +1,12 @@
 package kr.co.spotbuddy.application
 
 import kr.co.spotbuddy.domain.Alarm
+import kr.co.spotbuddy.domain.Member
 import kr.co.spotbuddy.domain.repository.AlarmRepository
 import kr.co.spotbuddy.exception.CustomException
 import kr.co.spotbuddy.exception.ExceptionDefinition
+import kr.co.spotbuddy.interfaces.request.AlarmRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -17,9 +20,28 @@ class AlarmService(
         alarmRepository.findByIdOrNull(id) ?: throw CustomException(ExceptionDefinition.BAD_REQUEST)
 
     @Transactional
+    fun saveAlarm(alarmRequest: AlarmRequest) {
+        val member = memberService.getById(alarmRequest.memberId)
+
+        alarmRepository.save(Alarm.from(alarmRequest, member))
+    }
+
+    @Transactional
     fun deleteAllAlarmsByMember(memberId: Long) {
         val member = memberService.getById(memberId)
         alarmRepository.deleteAllByMember(member)
+    }
+
+    @Transactional
+    fun changeAlarmStatusToRead(id: Long) {
+        val alarm = getAlarmById(id)
+
+        alarm.changeStatusToRead()
+    }
+
+    fun getMemberAlarms(memberId: Long, pageable: Pageable): Pair<List<Alarm>, Member> {
+        val member = memberService.getById(memberId)
+        return Pair(alarmRepository.findAllByMemberOrderByIdDesc(member, pageable).content, member)
     }
 
     @Transactional
@@ -29,4 +51,8 @@ class AlarmService(
         alarmRepository.delete(alarm)
     }
 
+    // TODO: Chat 구현 뒤 고치기
+    fun isAnyAlarmExists(memberId: Long) {
+
+    }
 }
